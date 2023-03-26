cb5e2ffc2f897039909f646f5bad18b837da18b9
rlronan <37887237+rlronan@users.noreply.github.com>
2023-03-26 15:12:15 -0400

    Update Django, add gitignore, master-->main

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -1,8 +1,8 @@
 # helloworld/urls.py
-from django.conf.urls import url
+from django.urls import path
 from django.conf.urls.static import static
 from helloworld import views
 
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view()),
+    path('', views.HomePageView.as_view()),
 ]
