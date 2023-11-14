3745cd0556203e5676cf3741b4cdf43079c013f0
rlronan <37887237+rlronan@users.noreply.github.com>
2023-11-14 17:12:06 -0500

    Update setting for Heroku hosting

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import django_on_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -160,3 +160,6 @@ LOGGING = {
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+
+
+django_on_heroku.settings(locals())
