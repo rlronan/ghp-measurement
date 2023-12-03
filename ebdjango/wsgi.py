dfd482a758de8ca2820155079c5a6750447c9972
rlronan <37887237+rlronan@users.noreply.github.com>
2023-12-03 16:10:32 -0500

    Still attempting to fix staticfiles for deployment on heroku

--- ebdjango/wsgi.py
+++ ebdjango/wsgi.py
@@ -10,8 +10,10 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
+#from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ebdjango.settings")
 
-application = StaticFilesHandler(get_wsgi_application())
+application = get_wsgi_application()
+
+# application = StaticFilesHandler(get_wsgi_application())
