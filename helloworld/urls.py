92842062e5cc5af81fe8b33b399b3f5e1aa3886c
rlronan <37887237+rlronan@users.noreply.github.com>
2023-12-03 15:38:07 -0500

    Update static root in attempt to fix staging, finally remove helloworld/

--- helloworld/urls.py
+++
@@ -1,8 +0,0 @@
-# helloworld/urls.py
-from django.urls import path
-from django.conf.urls.static import static
-from helloworld import views
-
-urlpatterns = [
-    path('', views.HomePageView.as_view()),
-]
