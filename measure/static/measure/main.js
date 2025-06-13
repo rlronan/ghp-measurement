44dcea3335ac68f251692061f13b48f9d1a465f5
Robert Ronan <rlronan@gmail.com>
2025-06-13 12:56:22 -0400

    Add buttons for stripe to mulitple pages. Remove old unused files

--- measure/static/measure/main.js
+++ measure/static/measure/main.js
@@ -11,6 +11,44 @@ fetch("/stripe_config/")
   
 // new
   // Event handler
+  // document.querySelector("#submitBtn").addEventListener("click", () => {
+  //   // Get Checkout Session ID
+  //   fetch("/create-checkout-session/")
+  //   .then((result) => { return result.json(); })
+  //   .then((data) => {
+  //     console.log(data);
+  //     // Redirect to Stripe Checkout
+  //     return stripe.redirectToCheckout({sessionId: data.sessionId})
+  //   })
+  //   .then((res) => {
+  //     console.log(res);
+  //   });
+  // });
+// Event handler for menu button
+  document.querySelector("#submit-menu-button").addEventListener("click", () => {
+    // Get Checkout Session ID
+    fetch("/create-checkout-session/")
+    .then((result) => { return result.json(); })
+    .then((data) => {
+      console.log(data);
+      // Redirect to Stripe Checkout
+      return stripe.redirectToCheckout({sessionId: data.sessionId})
+    })
+    .then((res) => {
+      console.log(res);
+    });
+  });
+});
+
+
+fetch("/stripe_config/")
+.then((result) => { return result.json(); })
+.then((data) => {
+  // Initialize Stripe.js
+  const stripe = Stripe(data.publicKey);
+  
+// new
+  // Event handler
   document.querySelector("#submitBtn").addEventListener("click", () => {
     // Get Checkout Session ID
     fetch("/create-checkout-session/")
@@ -24,8 +62,42 @@ fetch("/stripe_config/")
       console.log(res);
     });
   });
-// Event handler 2
-  document.querySelector("#submitBtn2").addEventListener("click", () => {
+});
+
+
+fetch("/stripe_config/")
+.then((result) => { return result.json(); })
+.then((data) => {
+  // Initialize Stripe.js
+  const stripe = Stripe(data.publicKey);
+  
+// new
+  // Event handler
+  document.querySelector("#submit-measure-button").addEventListener("click", () => {
+    // Get Checkout Session ID
+    fetch("/create-checkout-session/")
+    .then((result) => { return result.json(); })
+    .then((data) => {
+      console.log(data);
+      // Redirect to Stripe Checkout
+      return stripe.redirectToCheckout({sessionId: data.sessionId})
+    })
+    .then((res) => {
+      console.log(res);
+    });
+  });
+});
+
+
+fetch("/stripe_config/")
+.then((result) => { return result.json(); })
+.then((data) => {
+  // Initialize Stripe.js
+  const stripe = Stripe(data.publicKey);
+  
+// new
+  // Event handler
+  document.querySelector("#submit-measure-edit-button").addEventListener("click", () => {
     // Get Checkout Session ID
     fetch("/create-checkout-session/")
     .then((result) => { return result.json(); })
